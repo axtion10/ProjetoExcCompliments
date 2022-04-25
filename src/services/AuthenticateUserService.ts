542e3f7407ddbb27ce201b9d14cd584a5a4c7273
Gustavo Ferreira <gustavofvv@gmail.com>
2022-04-25 16:48:36 -0300

    feat: add new flow

--- src/services/AuthenticateUserService.ts
+++ src/services/AuthenticateUserService.ts
@@ -3,41 +3,41 @@ import { UsersRepositories } from "../repositories/UserRepositories";
 import { compare } from "bcryptjs";
 import { sign } from "jsonwebtoken";
 
-
 interface IAuthenticateRequest {
   email: string;
   password: string;
 }
 
-
 class AuthenticateUserService {
-
-  async execute( {email, password}: IAuthenticateRequest){
+  async execute({ email, password }: IAuthenticateRequest) {
     const usersRepositories = getCustomRepository(UsersRepositories);
 
     const user = await usersRepositories.findOne({
-      email
-    })
+      email,
+    });
 
-    if(!user) {
-      throw new Error("Email/Password incorrect")
+    if (!user) {
+      throw new Error("Email/Password incorrect");
     }
-    
+
     const passwordMatch = await compare(password, user.password);
 
-    if(!passwordMatch){
-      throw new Error("Email/Password incorrect")
+    if (!passwordMatch) {
+      throw new Error("Email/Password incorrect");
     }
 
-      const token = sign({
-        email: user.email
-      }, "f5a5c939eb7b3efe2d6885d0707fe542", {
+    const token = sign(
+      {
+        email: user.email,
+      },
+      "f5a5c939eb7b3efe2d6885d0707fe542",
+      {
         subject: user.id,
         expiresIn: "1d",
-      } );
-      return token;
+      }
+    );
+    return token;
   }
-
 }
 
 export { AuthenticateUserService };
